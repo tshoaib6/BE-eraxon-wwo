fe4eb9db53e11e9e4ba4bb6ec91dfbe19e6fc0cf
Shahid Ullah <114725262+ShahidUllahSE@users.noreply.github.com>
2024-10-22 07:29:41 +0500

    cleaned code
    cleaned code of stepform related files

--- src/routes/steps.routes.ts
+++ src/routes/steps.routes.ts
@@ -8,22 +8,3 @@ router.post('/steps', verifyJwt, createOrUpdateStep);
 
 export default router;
 
-
-// import express from 'express';
-// import { createOrUpdateStep } from '../controllers/steps.controller';
-// import { upload } from '../middleware/cloudinary'; // Import the upload middleware
-
-// const router = express.Router();
-
-// // Assuming the image files for the family members are uploaded under fields 'survivingFamilyMembers', 'predeceasedFamilyMembers', and 'fileSrc' for step5
-// router.post(
-//   '/stepform',
-//   upload.fields([
-//     { name: 'survivingFamilyMembers', maxCount: 10 },
-//     { name: 'predeceasedFamilyMembers', maxCount: 10 },
-//     { name: 'fileSrc', maxCount: 1 }, // fileSrc for step5
-//   ]),
-//   createOrUpdateStep
-// );
-
-// export default router;
