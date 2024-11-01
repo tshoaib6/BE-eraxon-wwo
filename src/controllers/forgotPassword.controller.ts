7cc73d6890e1c4f0593a65a61fa48fd7d907a7d6
Shahid Ullah <114725262+ShahidUllahSE@users.noreply.github.com>
2024-11-02 01:24:59 +0500

    made forgot password apis too

--- src/controllers/forgotPassword.controller.ts
+++ src/controllers/forgotPassword.controller.ts
@@ -1,12 +1,33 @@
 import { Request, Response } from 'express';
 import { forgotPasswordService } from '../services/forgotPassword.service';
 
-export const requestPasswordReset = async (req: Request, res: Response ) => {
-  const { email } = req.body;
+export const requestPasswordReset = async (req: Request, res: Response) => {
+  const email = req.query.email as string; // Get email from query
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required." });
+  }
 
   try {
     await forgotPasswordService.requestReset(email);
-  } catch (error:any) {
+    return res.status(200).json({ message: "OTP sent to email." });
+  } catch (error: any) {
     return res.status(400).json({ message: error.message });
+  }
+};
+
+
+export const verifyOTP = async (req: Request, res: Response) => {
+  const { email, otp } = req.body;
+
+  try {
+    const isValid = await forgotPasswordService.verifyOTP(email, otp);
+    if (isValid) {
+      res.status(200).json({ message: 'OTP verified successfully. You may proceed to reset your password.' });
+    } else {
+      res.status(400).json({ message: 'Invalid or expired OTP.' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
--- src/routes/forgotPassword.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { requestPasswordReset } from '../controllers/forgotPassword.controller';
-
-const router = express.Router();
-
-router.get('/requestReset', requestPasswordReset);
-
-
-export default router;
